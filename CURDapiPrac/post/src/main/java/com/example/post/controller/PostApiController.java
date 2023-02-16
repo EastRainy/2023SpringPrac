f8a90eb34fc2f3777c7e6b2eead8ff53e849adfb
EastRainy <48935971+EastRainy@users.noreply.github.com>
2023-02-16 15:46:39 +0900

    post project done

--- CURDapiPrac/post/src/main/java/com/example/post/controller/PostApiController.java
+++ CURDapiPrac/post/src/main/java/com/example/post/controller/PostApiController.java
@@ -1,4 +1,37 @@
 package com.example.post.controller;
 
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData) {
+
+        System.out.println(requestData.toString());
+
+    }
+
 }
+
+/*
+{
+    "account" : "user01",
+    "email" : "test@gmail.com",
+    "address" : "Seoul",
+    "password" : "1q2w3e4r!",
+    "phone_number" : "111-222-3333",
+    "OTP" : "12345"
+}
+
+
+
+
+ */
